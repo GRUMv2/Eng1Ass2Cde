bbbdd422ede52fb214f04679c1fc22e2fe918a12
Alex <code-signing@actorpus.com>
2024-11-30 17:00:28 +0000

    # Alex 30/11 - re-constructed LibGDX project to check for changes 	- Minor changes to window configuration in lwjgl3Launcher - Refactored project to io.github.grumv2.engsim 	- Introduced -.renderer hoping to condense rendering code into that when refactoring - fixed minor git issues, updated tracking and gitignore

--- core/src/main/java/github/game/Entity.java
+++
@@ -1,7 +0,0 @@
-package github.game;
-
-public abstract class Entity {
-
-    public abstract void update(Renderer renderer, InputHandler inputHandler);
-
-}
---
+++ lwjgl3/src/main/java/io/github/GRUMv2/EngSim/lwjgl3/Lwjgl3Launcher.java
@@ -0,0 +1,31 @@
+package io.github.GRUMv2.EngSim.lwjgl3;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import io.github.GRUMv2.EngSim.GameWindow;
+
+/** Launches the desktop (LWJGL3) application. */
+public class Lwjgl3Launcher {
+    public static void main(String[] args) {
+        if (StartupHelper.startNewJvmIfRequired()) return; // This handles macOS support and helps on Windows.
+        createApplication();
+    }
+
+    private static Lwjgl3Application createApplication() {
+        return new Lwjgl3Application(new GameWindow(), getDefaultConfiguration());
+    }
+
+    private static Lwjgl3ApplicationConfiguration getDefaultConfiguration() {
+        Lwjgl3ApplicationConfiguration configuration = new Lwjgl3ApplicationConfiguration();
+
+        configuration.setTitle("EngSim - University Simulator");
+
+        configuration.useVsync(true);
+        configuration.setForegroundFPS(Lwjgl3ApplicationConfiguration.getDisplayMode().refreshRate + 1);
+        configuration.setWindowedMode(GameWindow.WIDTH, GameWindow.HEIGHT);
+        configuration.setResizable(false);
+        configuration.setWindowIcon("libgdx128.png", "libgdx64.png", "libgdx32.png", "libgdx16.png");
+
+        return configuration;
+    }
+}
