64df0562484282b97268e24b47166b2199d7b0bb
alex <code-signing@actorpus.com>
2024-12-09 23:00:07 +0000

    Testing! Added Junit5 (5 is important 4 syntax is quite diferent) and written a baisc test suite for the event handler

--- game/core/src/main/java/io/github/GRUMv2/EngSim/Server/EventHandler/Event.java
+++ game/core/src/main/java/io/github/GRUMv2/EngSim/Server/EventHandler/Event.java
@@ -1,6 +1,6 @@
 package io.github.GRUMv2.EngSim.Server.EventHandler;
 
 
-abstract class Event {
+public abstract class Event {
     abstract public boolean tick(double delta, EventHandler handler);
 }
