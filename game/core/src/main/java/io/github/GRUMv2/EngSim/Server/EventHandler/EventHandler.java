86792f3dd39a846037af8745b289946c4c5c231d
alex <code-signing@actorpus.com>
2024-12-09 23:00:07 +0000

    Testing! Added Junit5 (5 is important 4 syntax is quite diferent) and written a baisc test suite for the event handler

--- game/core/src/main/java/io/github/GRUMv2/EngSim/Server/EventHandler/EventHandler.java
+++ game/core/src/main/java/io/github/GRUMv2/EngSim/Server/EventHandler/EventHandler.java
@@ -9,10 +9,10 @@ public class EventHandler {
     public EventHandler() {
         System.out.println("[ EVN ] EventHandler created");
 
-        // All events to be added here
-        activeEvents.add(new InitialiseTestEvent());
-        System.out.println("[ EVN ] 'InitialiseTestEvent' events created");
-        // TODO: look into way of automating this, gradle perhaps?
+//        // All events to be added here
+//        activeEvents.add(new InitialiseTestEvent());
+//        System.out.println("[ EVN ] 'InitialiseTestEvent' events created");
+//        // TODO: look into way of automating this, gradle perhaps?
     }
 
     void getGrid(int x, int y) {
---
+++ game/core/src/test/java/EventHandlerTest.java
@@ -0,0 +1,72 @@
+import io.github.GRUMv2.EngSim.Server.EventHandler.EventHandler;
+import io.github.GRUMv2.EngSim.Server.EventHandler.Event;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Field;
+import java.util.List;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/*
+Testing for:
+- Events that should be removed are removed
+- Events that should not be removed are not removed
+ */
+
+class InitialiseRemoveTestEvent extends Event {
+    @Override
+    public boolean tick(double delta, EventHandler handler) {
+        return false;
+    }
+}
+
+class InitialiseRemainTestEvent extends Event {
+    @Override
+    public boolean tick(double delta, EventHandler handler) {
+        return true;
+    }
+}
+
+
+public class EventHandlerTest {
+    private EventHandler eventHandler;
+
+    @BeforeEach
+    public void setUp() {
+        eventHandler = new EventHandler();
+    }
+
+    @Test
+    public void initialiseTestEventIsRemovedAfterTick() throws NoSuchFieldException, IllegalAccessException {
+        Field activeEventsField = EventHandler.class.getDeclaredField("activeEvents");
+        activeEventsField.setAccessible(true);
+        List<InitialiseRemoveTestEvent> activeEvents = (List<InitialiseRemoveTestEvent>) activeEventsField.get(eventHandler);
+
+        // Remove all events and add just the test case event
+        activeEvents.clear();
+        activeEvents.add(new InitialiseRemoveTestEvent());
+        assertEquals(1, activeEvents.size());
+
+        eventHandler.tick(0.016);
+
+        assertEquals(0, activeEvents.size());
+    }
+
+    @Test
+    public void remainEventTestIsNotRemovedAfterTick() throws NoSuchFieldException, IllegalAccessException {
+        Field activeEventsField = EventHandler.class.getDeclaredField("activeEvents");
+        activeEventsField.setAccessible(true);
+        List<InitialiseRemainTestEvent> activeEvents = (List<InitialiseRemainTestEvent>) activeEventsField.get(eventHandler);
+
+        // Remove all events and add just the test case event
+        activeEvents.clear();
+        activeEvents.add(new InitialiseRemainTestEvent());
+        assertEquals(1, activeEvents.size());
+
+        eventHandler.tick(0.016);
+
+        assertEquals(1, activeEvents.size());
+    }
+}
