9623366c51a50b4a4c2ccc67f6354276abd2752c
chmod400 <146387452+chmod400@users.noreply.github.com>
2024-12-23 22:19:08 +0000

    feat(entities): Leaderboard entity

--- game/core/src/main/java/io/github/GRUMv2/EngSim/client/MenuScreen.java
+++ game/core/src/main/java/io/github/GRUMv2/EngSim/client/MenuScreen.java
@@ -5,10 +5,25 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Align;
 
+import io.github.GRUMv2.EngSim.entities.Leaderboard;
+
+// >"MenuScreen"
+// >No menu
+
+/**
+ * MenuScreen
+ */
 public class MenuScreen extends AbstractGameScreen {
+
+    private Leaderboard leaderboard;
 
     public MenuScreen(Renderer renderer, InputHandler inputHandler) {
         super(renderer, inputHandler);
+        // TODO: unhardcode
+        this.leaderboard = new Leaderboard(
+            new Vector2(440, 40),
+            new Vector2(400, 280)
+        );
     }
 
     public void update(Renderer renderer, InputHandler inputHandler) {
@@ -17,6 +32,7 @@ public class MenuScreen extends AbstractGameScreen {
             this.changeEvent(Screens.GAME);
         }
 
-        renderer.drawText("Click anywhere to start", new Vector2(640, 360), Color.BLACK, 2f, Align.center);
+        renderer.drawText("Click anywhere to start", new Vector2(640, 540), Color.BLACK, 2f, Align.center);
+        this.leaderboard.update(renderer, inputHandler);
     }
 }
