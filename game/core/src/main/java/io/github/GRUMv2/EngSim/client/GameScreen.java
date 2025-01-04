2ceceb94b05c8871ab5db95e9df8dff9592c160f
alex <code-signing@actorpus.com>
2025-01-04 14:12:01 +0000

    patched server start into GameScreen

--- game/core/src/main/java/io/github/GRUMv2/EngSim/client/GameScreen.java
+++ game/core/src/main/java/io/github/GRUMv2/EngSim/client/GameScreen.java
@@ -10,6 +10,8 @@ import io.github.GRUMv2.EngSim.entities.BuildingFactory.Available;
 import io.github.GRUMv2.EngSim.entities.GameMap;
 import io.github.GRUMv2.EngSim.entities.UI;
 
+import io.github.GRUMv2.EngSim.Server.Server;
+
 public class GameScreen extends AbstractGameScreen {
 
     private Available buildingToPlace = null;
