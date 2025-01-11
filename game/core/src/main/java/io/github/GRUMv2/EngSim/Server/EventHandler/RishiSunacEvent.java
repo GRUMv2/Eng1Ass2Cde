2149ba01ace46508db68db567ac78f741feecac4
alex <code-signing@actorpus.com>
2025-01-11 16:41:09 +0000

    Having discussed with others, the rishi sunac event should occur 1 in 9 games.

--- game/core/src/main/java/io/github/GRUMv2/EngSim/Server/EventHandler/RishiSunacEvent.java
+++ game/core/src/main/java/io/github/GRUMv2/EngSim/Server/EventHandler/RishiSunacEvent.java
@@ -3,11 +3,11 @@ package io.github.GRUMv2.EngSim.Server.EventHandler;
 import java.util.Random;
 
 public class RishiSunacEvent extends Event {
-        private final Random random = new Random();
+    private final Random random = new Random();
 
     @Override
     public boolean tick(double delta, EventHandler handler) {
-        if (random.nextInt(0, 1_000_000) < 1) {
+        if (random.nextInt(0, 1_000_000) < 3) {
             // 'income' is a generated property, to tank the income
             // we need to set the monthlyUpkeepCostsMultiuplyer to a high value
             server.setMonthlyUpkeepCostsMultiuplyer(10);
