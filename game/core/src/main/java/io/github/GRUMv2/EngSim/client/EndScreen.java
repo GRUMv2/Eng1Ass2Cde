94aeed6dc5c673e7648590183decfb002689d49f
chmod400 <146387452+chmod400@users.noreply.github.com>
2025-01-11 18:37:40 +0000

    feat(EndScreen): Show stats/score on end screen

--- game/core/src/main/java/io/github/GRUMv2/EngSim/client/EndScreen.java
+++ game/core/src/main/java/io/github/GRUMv2/EngSim/client/EndScreen.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.utils.Align;
 
 import io.github.GRUMv2.EngSim.broker.Broker;
 import io.github.GRUMv2.EngSim.entities.Leaderboard;
+import io.github.GRUMv2.EngSim.entities.StatsBox;
 import io.github.GRUMv2.EngSim.entities.SubmitBox;
 import io.github.GRUMv2.EngSim.entities.TmpButton;
 
@@ -21,16 +22,21 @@ import io.github.GRUMv2.EngSim.entities.TmpButton;
 
 public class EndScreen extends AbstractGameScreen {
 
+    private Broker broker;
+
     private Leaderboard leaderboard;
+    private StatsBox statsBox;
     private SubmitBox submitBox;
     private TmpButton quitButton;
 
     public EndScreen(Renderer renderer, InputHandler inputHandler) {
         super(renderer, inputHandler);
-        // TODO: unhardcode
 
+        broker = Broker.getInstance();
+
+        // TODO: unhardcode
         this.submitBox = new SubmitBox(
-            new Vector2(70, 440),
+            new Vector2(70, 400),
             new Vector2(400, 40),
             () -> this.submitScore()
         );
@@ -40,6 +46,20 @@ public class EndScreen extends AbstractGameScreen {
             new Vector2(400, 280)
         );
         this.leaderboard.setValues(new ArrayList<SimpleImmutableEntry<String, Integer>>(Broker.getInstance().getLeaderboard()));
+
+        this.statsBox = new StatsBox(
+            new Vector2(500, 360),
+            new Vector2(280, 160)
+        );
+
+        this.statsBox.setStats(
+            broker.getMoney(),
+            broker.getIncome(),
+            broker.getStudentNumbers(),
+            broker.getStudentSatisfaction(),
+            broker.getStaffNumbers(),
+            broker.getStaffSatisfaction()
+        );
 
         this.quitButton = new TmpButton(
             "QUIT",
@@ -74,6 +94,9 @@ public class EndScreen extends AbstractGameScreen {
         renderer.drawText("Game Over", new Vector2(640, 580), Color.BLACK, 2f, Align.center);
         this.leaderboard.update(renderer, inputHandler);
         this.submitBox.update(renderer, inputHandler);
+        this.statsBox.update(renderer, inputHandler);
+        renderer.drawText("Score: " + Math.round(broker.getStudentSatisfaction() * 1000),
+            new Vector2(270, 500), Color.BLACK, 2f, Align.center);
         this.quitButton.update(renderer, inputHandler);
     }
 }
