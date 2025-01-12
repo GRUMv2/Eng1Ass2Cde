dd8bff6254efbb9a5843ad531c8b4200776982ae
alex <code-signing@actorpus.com>
2025-01-12 13:58:24 +0000

    Added changing the color of the buttons to better indicate selected mode

--- game/core/src/main/java/io/github/GRUMv2/EngSim/entities/TmpButton.java
+++ game/core/src/main/java/io/github/GRUMv2/EngSim/entities/TmpButton.java
@@ -4,8 +4,10 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Vector2;
 import io.github.GRUMv2.EngSim.client.InputHandler;
 import io.github.GRUMv2.EngSim.client.Renderer;
+import static io.github.GRUMv2.EngSim.client.GameScreen.Modes;
 
 public class TmpButton extends TmpTextBox {
+    public Modes mode = null;
 
     public TmpButton(String text, Vector2 pos, Vector2 size, Runnable handleClick) {
         super(pos, size);
@@ -18,9 +20,19 @@ public class TmpButton extends TmpTextBox {
         this.setBackgroundColor(backgroundColor);
     }
 
+    public TmpButton(String text, Vector2 pos, Vector2 size, Runnable handleClick, Modes mode) {
+        this(text, pos, size, handleClick);
+        this.mode = mode;
+    }
+
     // TODO: Button rendering abstraction
     @Override
     public void update(Renderer renderer, InputHandler inputHandler) {
+        update(renderer, inputHandler, this.getBackgroundColor());
+    }
+
+    public void update(Renderer renderer, InputHandler inputHandler, Color color) {
+        this.setBackgroundColor(color);
         super.update(renderer, inputHandler);
         if (inputHandler.getMouseClicked() && inputHandler.getMouseInBounds(this.getPos(), this.getSize())) {
             this.click();
--- game/core/src/main/java/io/github/GRUMv2/EngSim/entities/UI.java
+++ game/core/src/main/java/io/github/GRUMv2/EngSim/entities/UI.java
@@ -40,14 +40,14 @@ public class UI extends Entity {
                 new Vector2(20, 410),
                 new Vector2(250, 60),
                 () -> game.toggleMode(Modes.DESTROY),
-                Color.GRAY
+                Modes.DESTROY
             ),
             new TmpButton(
                 Modes.MOVE.toString(),
                 new Vector2(290, 410),
                 new Vector2(250, 60),
                 () -> game.toggleMode(Modes.MOVE),
-                Color.GRAY
+                Modes.MOVE
             )
         };
 
@@ -123,13 +123,13 @@ public class UI extends Entity {
         this.statsBox.update(renderer, inputHandler);
 
         for (TmpButton button : this.actionButtons) {
-            button.update(renderer, inputHandler);
+            button.update(renderer, inputHandler, game.getMode() == button.mode ? Color.GRAY : Color.LIGHT_GRAY);
         }
 
         // update the building place buttons
         for (BuildingPlaceButton buildingPlaceButton : buildingPlaceButtons) {
             buildingPlaceButton.setCount(broker.getBuildingCount(buildingPlaceButton.getBuildingType()));
-            buildingPlaceButton.update(renderer, inputHandler);
+            buildingPlaceButton.update(renderer, inputHandler, game.getBuildingToPlace() == buildingPlaceButton.getBuildingType());
         }
     }
 }
